17046dbacfa029c1cb8dff0e5958e6d3a40a2332
MrDylanWallace <70463207+MrDylanWallace@users.noreply.github.com>
2020-12-05 00:01:59 -0700

    Add files via upload

--- Java/cs2350_canvas.js
+++ Java/cs2350_canvas.js
@@ -2,5 +2,31 @@
     var c = document.getElementById("theCanvas");
     var ctx = c.getContext("2d");
     var img = document.getElementById("img");
-    ctx.drawImage(img, 10, 10, 150, 180);
+    ctx.drawImage(img, 10, 10, 500, 500);
   }
+
+  function animate() {
+    let rotation = (2 * Math.sin(counter / (3.14 * speed)));
+    context.setTransform(scale, 0, 0, scale, canvas.width / 2, canvas.height / 2);
+    context.clearRect(-canvas.width / 2, -canvas.height, canvas.width, canvas.height *2);
+    for (let i = 0; i < 360; i++) {
+      context.strokeStyle = colors[i % 6];
+      context.lineWidth = i / 100 + 1;
+      context.beginPath();
+      context.moveTo(0, 0);
+      context.lineTo(0, i);
+      context.stroke();
+      context.translate(0, i);
+      context.rotate((-60 + rotation) * 2 * Math.PI / 360);
+    }
+    window.requestAnimationFrame(animate);
+    counter++;
+  }
+   
+  let colors = ['blue', 'white', 'blue', 'white', 'blue', 'white'];
+  let canvas = document.getElementById('myCanvas');
+  let context = canvas.getContext('2d');
+  let counter = 0;
+  let scale = .3;
+  let speed = 20;
+  animate();
---
+++ Java/cs2350_linemaker.js
@@ -0,0 +1,11 @@
+﻿var dat = new Array(1,2,4,8,16,32,64,128,256);
+var c=document.getElementById("lineCanvas");
+var ctx=c.getContext("2d");
+//ctx.moveTo(0,250);
+ctx.moveTo(0,300-dat[0]);
+var j = 0;
+for(var i = 1; i < 9; i++){
+    j = j + 50;
+    ctx.lineTo(j,300-dat[i]);
+    ctx.stroke();
+}
